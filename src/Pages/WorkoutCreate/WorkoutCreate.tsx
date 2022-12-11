a4e42607ab6bc4a75431793431432c93f240df7d
Giovanni Rodighiero <giovanni.rodighiero@maieuticallabs.it>
2022-12-11 16:41:30 +0100

    fix workoutcreation types

--- src/Pages/WorkoutCreate/WorkoutCreate.tsx
+++ src/Pages/WorkoutCreate/WorkoutCreate.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import produce from "immer";
 
 import TopBar from "../../Components/TopBar";
 import WorkoutEdit from "../../Components/WorkoutEdit";
+import workoutEditReducer from "../../Components/WorkoutEdit/reducer";
 
 /**
  * Create workout page
  */
 function WorkoutCreate() {
+    const [workout, workoutDispatch] = React.useReducer(
+        produce(workoutEditReducer),
+        {
+            hasChanges: false,
+            id: 123,
+            name: "new workout",
+            rounds: [{ repeat: 1, break: 45, exercises: [] }],
+        }
+    );
     const navigate = useNavigate();
 
     const onWorkoutDelete = () => console.log("onDeleteWorkout");
@@ -22,13 +33,7 @@ function WorkoutCreate() {
                 onDelete={onWorkoutDelete}
                 title="New workout"
             />
-            <WorkoutEdit
-                originalWorkout={{
-                    name: "",
-                    id: 123,
-                    rounds: [{ exercises: [], repeat: 1, break: 20 }],
-                }}
-            />
+            <WorkoutEdit workout={workout} workoutDispatch={workoutDispatch} />
         </div>
     );
 }

d26aca31abe027f8dc85583560187cfeb4d75973
Giovanni Rodighiero <giovanni.rodighiero@outlook.it>
2023-07-29 21:25:38 +0200

    feat: update useTimer

--- src/Pages/WorkoutExecution/useTimer.ts
+++ src/Pages/WorkoutExecution/useTimer.ts
@@ -1,8 +1,10 @@
 import React from "react";
 
 import Workout from "../../types/workout.interface";
+import Round from "../../types/round.interface";
+import Exercise from "../../types/exercise.interface";
 
-enum PHASES {
+export enum PHASES {
     GET_READY,
     WORK,
     REST,
@@ -21,11 +23,26 @@ interface useTimerInterface {
     /** The current seconds remaining */
     value: number;
 
+    /** The current seconds remaining as percentage */
+    percentage: number;
+
+    /** Total seconds elapsed from the start of the timer */
+    elapsedTime: number;
+
     /** The current phase of the workout */
     phase: PHASES;
 
+    /** The loaded workout */
+    workout: Workout;
+
+    /** The current round of the workout */
+    round: Round;
+
+    /** The current exercise of the workout */
+    exercise: Exercise;
+
     /** The current (or next up) exercise name */
-    exercise: string;
+    exerciseName: string;
 
     /** The current exercise repetition */
     exerciseRepIndex: number;
@@ -65,6 +82,9 @@ function useTimer(
     const intervalRef = React.useRef<NodeJS.Timer>();
     const [workout, setWorkout] = React.useState<Workout>(initialWorkout);
     const [isRunning, setIsRunning] = React.useState<boolean>(false);
+    const [elapsedTime, setElapsedTime] = React.useState<number>(0);
+    const [startingValue, setStartingValue] =
+        React.useState<number>(GET_READY_DURATION);
     const [value, setValue] = React.useState<number>(GET_READY_DURATION);
     const [phase, setPhase] = React.useState<number>(PHASES.GET_READY);
 
@@ -79,10 +99,13 @@ function useTimer(
     const [exerciseName, setExerciseName] = React.useState<string>(
         exercise.name
     );
+    const percentage = parseFloat(((value / startingValue) * 100).toFixed(2));
 
     const resetTimer = () => {
         setIsRunning(false);
         setValue(GET_READY_DURATION);
+        setStartingValue(GET_READY_DURATION);
+        setElapsedTime(0);
         setPhase(PHASES.GET_READY);
         setRoundIndex(0);
         setRoundRepIndex(0);
@@ -95,6 +118,7 @@ function useTimer(
             case PHASES.GET_READY:
                 setPhase(PHASES.WORK);
                 setValue(exercise.work);
+                setStartingValue(exercise.work);
                 break;
 
             case PHASES.WORK:
@@ -105,6 +129,7 @@ function useTimer(
                 ) {
                     setPhase(PHASES.REST);
                     setValue(exercise.rest);
+                    setStartingValue(exercise.rest);
                     if (exerciseRepIndex === exercise.repeat - 1)
                         setExerciseName(
                             round.exercises[exerciseIndex + 1].name
@@ -116,6 +141,7 @@ function useTimer(
                 if (roundRepIndex < round.repeat - 1) {
                     setPhase(PHASES.BREAK);
                     setValue(round.break);
+                    setStartingValue(round.break);
                     setExerciseName(round.exercises[0].name);
                     break;
                 }
@@ -124,6 +150,7 @@ function useTimer(
                 if (roundIndex < workout.rounds.length - 1) {
                     setPhase(PHASES.BREAK);
                     setValue(round.break);
+                    setStartingValue(round.break);
                     setExerciseName(
                         workout.rounds[roundIndex + 1].exercises[0].name
                     );
@@ -140,6 +167,7 @@ function useTimer(
                 if (exerciseRepIndex < exercise.repeat - 1) {
                     setPhase(PHASES.WORK);
                     setValue(exercise.work);
+                    setStartingValue(exercise.work);
                     setExerciseRepIndex(prev => prev + 1);
                     break;
                 }
@@ -149,6 +177,7 @@ function useTimer(
                 setExerciseRepIndex(0);
                 setExerciseIndex(prev => {
                     setValue(round.exercises[prev + 1].work);
+                    setStartingValue(round.exercises[prev + 1].work);
                     return prev + 1;
                 });
                 break;
@@ -160,6 +189,7 @@ function useTimer(
                     setExerciseRepIndex(0);
                     setExerciseIndex(0);
                     setValue(round.exercises[0].work);
+                    setStartingValue(round.exercises[0].work);
                     setRoundRepIndex(prev => prev + 1);
                     break;
                 }
@@ -171,6 +201,9 @@ function useTimer(
                 setRoundRepIndex(0);
                 setRoundIndex(prev => {
                     setValue(workout.rounds[prev + 1].exercises[0].work);
+                    setStartingValue(
+                        workout.rounds[prev + 1].exercises[0].work
+                    );
                     return prev + 1;
                 });
                 break;
@@ -197,6 +230,7 @@ function useTimer(
         if (isRunning) {
             intervalRef.current = setInterval(() => {
                 setValue(prev => prev - 1);
+                setElapsedTime(prev => prev + 1);
             }, 1000);
         }
 
@@ -211,8 +245,13 @@ function useTimer(
         isReady: workout.id !== 0 && workout.name !== "default",
         isRunning,
         value,
+        percentage,
+        elapsedTime,
         phase,
-        exercise: exerciseName,
+        workout,
+        round,
+        exercise,
+        exerciseName,
         exerciseIndex,
         exerciseRepIndex,
         roundIndex,
